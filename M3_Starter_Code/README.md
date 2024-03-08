16616c1a55b2a7227074bfd9e45cb345c1a36555
2Hail <2Hail@protonmail.com>
2024-03-08 16:09:55 -0500

    Account.py

--- M3_Starter_Code/README.md
+++ M3_Starter_Code/README.md
@@ -1,3 +1,5 @@
 # customer_banking
 
-In this customer_banking challennge we will be importing files to a "main" file to which we will be prompting the user with calculations to which solve for variables such as interest and updated balances.
+In this customer_banking challennge we will be importing files to a "main" file to which we will be prompting the user with calculations which solve for variables such as interest and updated balances.
+
+The applications of this exercise are with a Savings Account, as well as a CD account. There, we take the initial values implemented by the customer, and calculate the yield on each respective account.
--- M3_Starter_Code/customer_banking.py
+++ M3_Starter_Code/customer_banking.py
@@ -1,6 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
-
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 # Define the main function
 def main():
     """This function prompts the user to enter the savings and cd account balance, interest rate,
@@ -9,21 +10,38 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
+    savings_balance = float(input("Please enter savings account balance: "))
+    savings_interest = float(input("Please enter savings account interest rate: "))
+    savings_maturity = int(input("Please enter months for savings maturity: "))
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    """Here is the Hundreths, 2 decimal places"""
+    print("The interest you have earned on the savings account is: ", "{:.2f}".format(interest_earned))
+    print("The updated savings account balance is: ", "{:.2f}".format(updated_savings_balance))
+    print(f'\n')
+    """Here is the Thousandths, 3 decimal places"""
+    print("The interest you have earned on the savings account is (thousandths): ", "{:.3f}".format(interest_earned))
+    print("The updated savings account balance is (thousandths): ", "{:.3f}".format(updated_savings_balance))
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-
+    cd_balance = float(input("Please enter the CD account balance: "))
+    cd_interest = float(input("Please enter the CD account interest rate: "))
+    cd_maturity = int(input("Please enter the months for CD maturity: "))
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    """Here is the Hundreths, 2 decimal places"""
+    print("The interest you have earned on the CD account is: ", "{:.2f}".format(interest_earned))
+    print("The updated CD account balance is: ", "{:.2f}".format(updated_cd_balance))
+    print(f'\n')
+    """Here is the Thousandths, 3 decimal places"""
+    print("The interest you have earned on the CD account is (thousandths): ", "{:.3f}".format(interest_earned))
+    print("The updated CD account balance is (thousandths): ", "{:.3f}".format(updated_cd_balance))
 if __name__ == "__main__":
     # Call the main function.
+    main()
